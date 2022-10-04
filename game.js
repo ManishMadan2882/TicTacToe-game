af298409400c39ff1beaa0aec6f8020cc1c6b4ae
Guillermo Otegui <guilleotegui03@gmail.com>
2022-10-04 18:40:02 -0300

    Add confetti when player win

--- game.js
+++ game.js
@@ -40,6 +40,20 @@ if(boxtexts[e[0]].innerText === boxtexts[e[1]].innerText && boxtexts[e[2]].inner
         boxes[e[i]].style.backgroundColor='rgb(174, 181, 249)';
         i++;
     },900)
+
+    const start = () => {
+      setTimeout(function() {
+        confetti.start()
+      }, 1000); // 1000 is time that after 1 second start the confetti ( 1000 = 1 sec)
+    };
+    //  Stop
+    const stop = () => {
+      setTimeout(function() {
+        confetti.stop()
+      }, 5000); // 5000 is time that after 5 second stop the confetti ( 5000 = 5 sec)
+    };
+    start();
+    stop();
     
 }
  
