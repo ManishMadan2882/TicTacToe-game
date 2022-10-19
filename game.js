2b42788c976e7c2d0d41b324b829d0527b2e20d0
GAURCODER <lcs2021008@iiitl.ac.in>
2022-10-19 12:47:42 +0530

    #15 : ALgorithm for computer in the game

--- game.js
+++ game.js
@@ -1,5 +1,15 @@
 let boxes = document.getElementsByClassName("box");
 let turn = "X";
+let wins = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 let isGameOver = false;
 let turnCount = 0;
 let isDarkMode = false;
@@ -18,16 +28,7 @@ const changeTurn = () => {
 
 const checkWin = () => {
   let boxtexts = document.getElementsByClassName("boxtext");
-  let wins = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
+
   wins.forEach((e) => {
     if (
       boxtexts[e[0]].innerText === boxtexts[e[1]].innerText &&
@@ -70,14 +71,77 @@ Array.from(boxes).forEach((element) => {
       console.log(turnCount);
       if (!isGameOver) {
         turn = changeTurn();
+        console.log(turn);
         turnCount++;
         if (turnCount < 9) {
           document.querySelector(".info").innerText = "Turn for : " + turn;
+          setTimeout(function () {
+            computerTurn();
+          }, 400);
         } else document.querySelector(".info").innerText = "Draw";
       } else turn = "";
     }
   });
 });
+
+const computerTurn = () => {
+  let emptyCells = [];
+  let random;
+  let boxtexts = document.getElementsByClassName("boxtext");
+  for (let i = 0; i < boxtexts.length; i++) {
+    if (boxtexts[i].innerText === "") {
+      emptyCells.push(i);
+    }
+  }
+  let flag = 1;
+  let block;
+  wins.forEach((e) => {
+    if (
+      boxtexts[e[0]].innerText === boxtexts[e[1]].innerText &&
+      boxtexts[e[0]].innerText != "" &&
+      boxtexts[e[2]].innerText == ""
+    ) {
+      console.log("first", e[0], e[1]);
+      block = e[2];
+      flag = 0;
+    }
+    if (
+      boxtexts[e[0]].innerText === boxtexts[e[2]].innerText &&
+      boxtexts[e[0]].innerText != "" &&
+      boxtexts[e[1]].innerText == ""
+    ) {
+      console.log("second");
+      block = e[1];
+      flag = 0;
+    }
+    if (
+      boxtexts[e[2]].innerText === boxtexts[e[1]].innerText &&
+      boxtexts[e[1]].innerText != "" &&
+      boxtexts[e[0]].innerText == ""
+    ) {
+      console.log("third");
+      block = e[0];
+      flag = 0;
+    }
+  });
+  if (flag) {
+    random = Math.ceil(Math.random() * emptyCells.length) - 1;
+    boxtexts[emptyCells[random]].innerText = turn;
+  } else {
+    boxtexts[block].innerText = turn;
+    console.log(boxtexts[block]);
+  }
+  checkWin();
+
+  if (!isGameOver) {
+    turn = changeTurn();
+    turnCount++;
+    if (turnCount < 9) {
+      document.querySelector(".info").innerText = "Turn for : " + turn;
+    } else document.querySelector(".info").innerText = "Draw";
+  } else turn = "";
+};
+
 let resetBtn = document.getElementById("reset");
 resetBtn.addEventListener("click", () => {
   isGameOver = false;
